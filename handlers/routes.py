c61105986933dd2d07fcf9b74c7c0c99af572873
AlekseySolovev <alexsolovyev-2010@mail.ru>
2023-04-24 19:31:14 +0600

    add creating routes for contacts and information, add router for start_ask_user_to_use_buttons and for start main menu from callback

--- handlers/routes.py
+++ handlers/routes.py
@@ -11,3 +11,19 @@ def create_routes():
     
     from handlers.products import routes as products_routes
     products_routes.create_routes()
+
+    dp.register_message_handler(
+        handlers.start_ask_user_to_use_buttons,
+        content_types=['text'],
+    )
+
+    dp.register_callback_query_handler(
+        handlers.start_main_menu_from_callback,
+        text=['main_menu'],
+    )
+
+    from handlers.information import routes
+    routes.create_routes()
+
+    from handlers.contacts import routes
+    routes.create_routes()
